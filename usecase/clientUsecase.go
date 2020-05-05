5e62294a576b13892a2b17a873d40d6ba790f8fb
gamakireta <gamakireta@gmail.com>
2020-05-05 19:22:53 +0700

    clientUsecase.go : setting up client to be used by presenter
    Signed-off-by: gamakireta <gamakireta@gmail.com>

---
+++ usecase/clientUsecase.go
@@ -0,0 +1,56 @@
+package usecase
+
+import (
+	"strconv"
+
+	"../repository"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
+)
+
+func NewClient(c *gin.Context, cR *redis.Client) {
+
+	cN := c.Params.ByName("client")
+	if cN == "" {
+		repository.BadResponse(c, "no client")
+		return
+	}
+
+	tL := c.Params.ByName("ttl")
+	if tL == "" {
+		tL = "60"
+	}
+
+	d, err := repository.GetData(cN, cR)
+	if len(d) != 0 {
+		repository.BadResponse(c, "Failed")
+		return
+	}
+
+	tTL, err := strconv.Atoi(tL)
+	if err != nil {
+		tTL = 120
+	}
+
+	dJ, err := repository.BindJsonRequest(c)
+	err = repository.NewClientData(cR, dJ, cN, tTL)
+	if err != nil {
+		repository.BadResponse(c, err.Error())
+		return
+	}
+
+	repository.SuccessResponse(c, "success adding data")
+}
+
+func GetRedisData(c *gin.Context, cR *redis.Client) {
+	cN := c.Params.ByName("key")
+	dR, err := repository.GetData(cN, cR)
+	if err == redis.Nil {
+		repository.BadResponse(c, cN+" not exist")
+		return
+	} else if err != nil {
+		repository.BadResponse(c, err.Error())
+		return
+	}
+	repository.SuccessGettingData(c, dR)
+}
