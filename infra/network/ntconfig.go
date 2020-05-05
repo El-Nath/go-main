c9df7658c57cdbece5b2c0e0285e1258733e597c
gamakireta <gamakireta@gmail.com>
2020-05-05 09:24:28 +0700

    ntconfig.go : stitching up presenter into ntconfig
    Signed-off-by: gamakireta <gamakireta@gmail.com>

--- infra/network/ntconfig.go
+++ infra/network/ntconfig.go
@@ -2,14 +2,15 @@ package network
 
 import "github.com/gin-gonic/gin"
 import "../esconfig"
+import "../../presenter"
 
 func InitRoutes(env *esconfig.Env) *gin.Engine {
 
 	r := gin.Default()
 	vx := r.Group("api/vx")
 	{
-		vx.POST("/premeditated_data/:fakedata")
-		vx.GET("/search/:index")
+		vx.POST("/premeditated_data/:fakedata", presenter.InsertData(env))
+		vx.GET("/search/:index", presenter.ResultData(env))
 	}
 
 	return r
